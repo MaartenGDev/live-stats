f5c1e12d8bcbaf3c05b1b4e910aabd55bec892d7
MaartenGDev <maarten.grootoonk@gmail.com>
2017-02-21 22:09:51 +0100

    Added debug middleware

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : err;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
   res.render('error');
