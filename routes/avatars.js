c6f237775476f0547992448502bacbb1c73ce26a
MaartenGDev <maarten.grootoonk@gmail.com>
2017-03-11 22:05:31 +0100

    Added skin formatter

--- routes/avatars.js
+++ routes/avatars.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Jimp = require("jimp");
 const fs = require('fs');
+const download = require('download');
+const sharp = require('sharp');
 
 const fetch = require('node-fetch');
 
@@ -11,7 +13,6 @@ router.get('/:uuid', (req, res, next) => {
     const storePath = `${process.env.PWD}/storage/avatars/${uuid}.png`;
 
     fs.access(storePath, err => {
-        console.log(err)
         if (err) {
 
             getImageUriByUuid(uuid)
@@ -19,7 +20,7 @@ router.get('/:uuid', (req, res, next) => {
                     console.log('getting ' + uri)
 
                     saveSkinToFile(uri, storePath)
-                        .then(myUri => {
+                        .then(_ => {
                             console.log('save to file')
 
                             return res.sendFile(storePath);
@@ -50,25 +51,39 @@ const getImageUriByUuid = uuid => {
                 res(SKIN.url);
             }).catch(err => rej(err))
     });
-
 }
 
 const saveSkinToFile = (uri, storePath) => {
-    console.log(storePath)
-
     return new Promise((res, rej) => {
-        Jimp.read(uri)
-            .then(avatar => {
-                avatar.crop(8,8,8,8)
-                avatar.resize(90, 90)
-                    .write(storePath, (err, result) => {
-                        console.log(err, result)
-                        res(result)
+        download(uri).then(data => {
+            fs.writeFileSync(storePath + '-high.png', data);
+
+            Jimp.read(storePath + '-high.png').then(avatar => {
+                avatar
+                    .crop(8, 8, 8, 8)
+                    .write(storePath + '-formatted.png', _ => {
+                        Jimp.read(storePath + '-formatted.png').then(head => {
+                            head
+                                .scaleToFit(45, 45)
+                                .write(storePath + '-head.png', _ => {
+                                    Jimp.read(storePath + '-formatted.png').then(head2 => {
+                                        head2
+                                            .resize(45, Jimp.AUTO)
+                                            .write(storePath + '-head-AUTO.png');
+                                    });
+                                });
+                        });
                     });
 
+                //                     .scaleToFit(90, 90)
+
+
+
             }).catch(err => console.log(err));
+
+
+        });
     });
 }
-
 
 module.exports = router;
